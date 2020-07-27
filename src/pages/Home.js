87c06406695a7a32e3ab7fd974e973347d0fe0e5
jeromeg-fullstack <jeromegee@gmail.com>
2020-07-28 05:50:27 +0800

    Installed header slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/Header/index';
 import LoginRegister from "../components/LoginRegister";
 import MobileMenu from "../components/MobileMenu";
+import Slider from '../components/Slider/home';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header/>
+      <Slider/>
       <MobileMenu/>
       <LoginRegister/>
     </React.Fragment>
