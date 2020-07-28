dcd11c7ea0f495c10d87bf4404c9606926407990
jeromeg-fullstack <jeromegee@gmail.com>
2020-07-28 08:19:00 +0800

    Installed About Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import Header from '../components/Header/index';
 import LoginRegister from "../components/LoginRegister";
 import MobileMenu from "../components/MobileMenu";
 import Slider from '../components/Slider/home';
+import About from '../components/About/home';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header/>
       <Slider/>
+      <About/>
       <MobileMenu/>
       <LoginRegister/>
     </React.Fragment>
